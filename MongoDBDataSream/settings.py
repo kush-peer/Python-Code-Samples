8bd32a3f6d69f49d194d12e51c251197ab56544e
Kush Sharma <kush.sharma@peerislands.io>
2022-04-26 12:23:22 -0500

    Initial Check in change stream

---
+++ MongoDBDataSream/settings.py
@@ -0,0 +1,19 @@
+from ast import Str
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# Accessing variables.
+NUM_RECORDS_TO_GENERATE = 1000
+MONGODB_ATLAS_URL = 'mongodb+srv://mongodb-admin:mongodb-password@cayman.ya2tg.azure.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+DATABASE = 'retail'
+COLLECTION = 'superstore'
+
+# Using variables.
+print('Num Records to generate: ' + str(NUM_RECORDS_TO_GENERATE))
+print('MongoDB Atlas DB URL: ' + MONGODB_ATLAS_URL)
+print('Database: ' + DATABASE)
+print('Collection: ' + COLLECTION)
